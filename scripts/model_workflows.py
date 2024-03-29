723e63d298d0cb4aecf0d75a2f9cc14ddb845645
Hank Zhao <hankzhao32@gmail.com>
2024-03-29 13:06:45 -0700

    split the model function into different sub-functions

--- scripts/model_workflows.py
+++ scripts/model_workflows.py
@@ -15,17 +15,33 @@ from sklearn.compose import make_column_transformer
 @click.option('--save_image_path', type=str, help = 'File path to save all images', default='../images')
 @click.option('--save_table_path', type=str, help = 'File path to save all tables',default='../data/data_for_quarto')
 
+
 def main(parquet_path, save_image_path, save_table_path):
+    X_train, X_test, y_train, y_test = split_train_test(parquet_path)
+
+    ohe, scaler = preprocessing()
+
+    ct = transformer(ohe, scaler)
+
+    results = cross_validation_output(ct, X_train, y_train, save_table_path)
+
+    final_pipe = build_final_model(results, ct, X_train, y_train)
+    
+    evaluate_model(final_pipe, X_test, y_test, save_image_path)
+
+def split_train_test(parquet_path):
     data = pd.read_parquet(parquet_path)
     X = data.drop(columns = ['wicket'])
     y = data['wicket']
     X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=0.7, random_state=123)
+    return X_train, X_test, y_train, y_test
 
-
-
+def preprocessing():
     ohe = OneHotEncoder(drop = "if_binary", handle_unknown="ignore")
     scaler = StandardScaler()
+    return ohe, scaler
 
+def transformer(ohe, scaler): 
     numerical_feats = ['runs_cumulative']
     categorical_feats = ['inning', 'over', 'powerplay', 'over_ball',]
     drop_feats = ['game_id', 'season', 'team','batter', 'batter_id', 'bowler',
@@ -33,14 +49,16 @@ def main(parquet_path, save_image_path, save_table_path):
         'legbyes', 'byes', 'player_out', 'player_out_id', 'fielders_name',
         'fielders_id', 'wicket_type', 'runs_batter', 'runs_extras', 
         'runs_total', 'team_over']
-
-
+    
     ct = make_column_transformer(
         (scaler, numerical_feats), 
         (ohe, categorical_feats),
         ("drop", drop_feats)
     )
 
+    return ct
+
+def cross_validation_output(ct, X_train, y_train, save_table_path):
     C = [10 ** x for x in [0.5, 1, 2, 3, 4]]
     train_score = []
     cv_score = []
@@ -52,7 +70,9 @@ def main(parquet_path, save_image_path, save_table_path):
     results = pd.DataFrame({"C": C, "Training Accuracy": train_score, "Cross Validation Accuracy": cv_score})
     results.to_csv(os.path.join(save_table_path, "Hyperparameter.csv"))
 
+    return results
 
+def build_final_model(results, ct, X_train, y_train):
     results['Difference'] = results['Training Accuracy'] - results['Cross Validation Accuracy']
 
     least_overfit = results.loc[results['Difference'] > 0, 'Difference'].min()
@@ -65,6 +85,10 @@ def main(parquet_path, save_image_path, save_table_path):
         final_model
     )
     final_pipe.fit(X_train, y_train)
+
+    return final_pipe
+
+def evaluate_model(final_pipe, X_test, y_test, save_image_path):
     score = final_pipe.score(X_test, y_test)
     conf_mat = metrics.confusion_matrix(y_test, final_pipe.predict(X_test))
     plot_cm = metrics.ConfusionMatrixDisplay(conf_mat)
@@ -72,6 +96,7 @@ def main(parquet_path, save_image_path, save_table_path):
     plt.savefig(os.path.join(save_image_path, "chart7.png"))
     print(f"Model Score: {score}")
     print(f"Chart saved to: {save_image_path}")
+
 
 if __name__ == '__main__':
     main()
